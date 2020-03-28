079cd053ea2db57084b3d0439828a07b272c88f4
Lukas Kirner <lkirner@inovex.de>
2020-03-28 14:24:26 +0100

    change to Parameterized tests

--- src/test/java/io/jenkins/plugins/grading/TestScoreTest.java
+++ src/test/java/io/jenkins/plugins/grading/TestScoreTest.java
@@ -1,51 +1,77 @@
 package io.jenkins.plugins.grading;
 
-import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 
 import hudson.tasks.junit.TestResultAction;
 
-import static io.jenkins.plugins.grading.assertions.Assertions.*;
-import static org.mockito.Mockito.*;
+import static io.jenkins.plugins.grading.assertions.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Tests the class {@link TestScore}.
  *
  * @author Eva-Maria Zeintl
  * @author Ullrich Hafner
+ * @author Lukas Kirner
  */
-class TestScoreTest {
-    @Test
-    void shouldCalculate() {
-        TestConfiguration testsConfiguration = new TestConfiguration.TestConfigurationBuilder().setMaxScore(25)
-                .setSkippedImpact(-1)
-                .setFailureImpact(-2)
-                .setPassedImpact(1)
-                .build();
 
-        TestResultAction action = createAction(8, 1, 1);
-        TestScore test = new TestScore(testsConfiguration, action);
+@RunWith(Parameterized.class)
+public class TestScoreTest {
 
-        assertThat(test.getTotalImpact()).isEqualTo(3);
+    @Parameter
+    public TestConfiguration configuration;
+
+    @Parameter(1)
+    public TestResultAction resultAction;
+
+    @Parameter(2)
+    public int expect;
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+            {
+                createTestConfiguration(25,-1, -2, 1),
+                createAction(8, 1, 1),
+                3
+            },
+            {
+                createTestConfiguration(25,-1, -2, 1),
+                createAction(8, 5, 1),
+                -9
+            },
+        });
     }
 
-    private TestResultAction createAction(final int totalSize, final int failedSize, final int skippedSize) {
+    @Test
+    public void test() {
+        TestScore test = new TestScore(configuration, resultAction);
+        assertThat(test.getTotalImpact()).isEqualTo(expect);
+    }
+
+    private static TestConfiguration createTestConfiguration(
+            final int maxScore, final int skippedImpact, final int failureImpact, final int passedImpact) {
+        return new TestConfiguration.TestConfigurationBuilder()
+            .setMaxScore(maxScore)
+            .setSkippedImpact(skippedImpact)
+            .setFailureImpact(failureImpact)
+            .setPassedImpact(passedImpact)
+            .build();
+    }
+
+    private static TestResultAction createAction(final int totalSize, final int failedSize, final int skippedSize) {
         TestResultAction action = mock(TestResultAction.class);
         when(action.getTotalCount()).thenReturn(totalSize);
         when(action.getFailCount()).thenReturn(failedSize);
         when(action.getSkipCount()).thenReturn(skippedSize);
         return action;
-    }
-
-    @Test
-    void shouldCalculateNegativeResult() {
-        TestConfiguration testsConfiguration = new TestConfiguration.TestConfigurationBuilder().setMaxScore(25)
-                .setSkippedImpact(-1)
-                .setFailureImpact(-2)
-                .setPassedImpact(1)
-                .build();
-
-        TestScore test = new TestScore(testsConfiguration, createAction(8, 5, 1));
-
-        assertThat(test.getTotalImpact()).isEqualTo(-9);
     }
 }
