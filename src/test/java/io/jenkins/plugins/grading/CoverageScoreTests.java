08a52e78b49d0ed3ee403050d71e24609e0fa10c
Patrick Rogg <p_rogg@t-online.de>
2020-03-28 16:39:14 +0100

    Add missing Tests for CoverageScore

--- src/test/java/io/jenkins/plugins/grading/CoverageScoreTests.java
+++ src/test/java/io/jenkins/plugins/grading/CoverageScoreTests.java
@@ -2,8 +2,6 @@ package io.jenkins.plugins.grading;
 
 import org.junit.jupiter.api.Test;
 
-import hudson.model.TaskListener;
-
 import io.jenkins.plugins.coverage.targets.Ratio;
 import io.jenkins.plugins.grading.CoverageConfiguration.CoverageConfigurationBuilder;
 
@@ -14,15 +12,74 @@ import static io.jenkins.plugins.grading.assertions.Assertions.*;
  *
  * @author Eva-Maria Zeintl
  * @author Ullrich Hafner
+ * @author Patrick Rogg
  */
 class CoverageScoreTests {
+
     @Test
-    void shouldCalculate() {
-        CoverageConfiguration coverageConfiguration = new CoverageConfigurationBuilder()
-                .setMissedImpact(-2)
-                .build();
-        CoverageScore coverageScore = new CoverageScore(coverageConfiguration, Ratio.create(99, 100));
+    void shouldCalculateTotalImpactWithZeroCoveredImpact() {
+        final CoverageConfiguration coverageConfiguration = createCoverageConfiguration(-2, 0);
+        final CoverageScore coverageScore = new CoverageScore(coverageConfiguration, Ratio.create(99, 100));
 
         assertThat(coverageScore).hasTotalImpact(-2);
+    }
+
+    @Test
+    void shouldCalculateTotalImpactWithZeroMissedImpact() {
+        final CoverageConfiguration coverageConfiguration = createCoverageConfiguration(0, 5);
+        final CoverageScore coverageScore = new CoverageScore(coverageConfiguration, Ratio.create(99, 100));
+
+        assertThat(coverageScore).hasTotalImpact(495);
+    }
+
+    @Test
+    void shouldCalculateTotalImpact() {
+        final CoverageConfiguration coverageConfiguration = createCoverageConfiguration(-1, 3);
+        final CoverageScore coverageScore = new CoverageScore(coverageConfiguration, Ratio.create(99, 100));
+
+        assertThat(coverageScore).hasTotalImpact(296);
+    }
+
+    @Test
+    void shouldGetId() {
+        final CoverageConfiguration coverageConfiguration = new CoverageConfigurationBuilder().build();
+        final CoverageScore coverageScore = new CoverageScore(coverageConfiguration, Ratio.create(99, 100));
+
+        assertThat(coverageScore.getId()).isEqualTo("Line");
+    }
+
+    @Test
+    void shouldGetTotalImpact() {
+        final CoverageConfiguration coverageConfiguration = createCoverageConfiguration(1, 1);
+        final CoverageScore coverageScore = new CoverageScore(coverageConfiguration, Ratio.create(99, 100));
+
+        final int missedImpact = coverageConfiguration.getMissedImpact() * coverageScore.getMissedSize();
+        final int coveredImpact = coverageConfiguration.getCoveredImpact() * coverageScore.getCoveredSize();
+        assertThat(coverageScore.getTotalImpact()).isEqualTo(missedImpact + coveredImpact);
+    }
+
+    @Test
+    void shouldGetCoveredSize() {
+        final Ratio codeCoverageRatio = Ratio.create(99, 100);
+        final CoverageConfiguration coverageConfiguration = new CoverageConfigurationBuilder().build();
+        final CoverageScore coverageScore = new CoverageScore(coverageConfiguration, codeCoverageRatio);
+
+        assertThat(coverageScore.getCoveredSize()).isEqualTo(codeCoverageRatio.getPercentage());
+    }
+
+    @Test
+    void shouldGetMissedSize() {
+        final Ratio codeCoverageRatio = Ratio.create(99, 100);
+        final CoverageConfiguration coverageConfiguration = new CoverageConfigurationBuilder().build();
+        final CoverageScore coverageScore = new CoverageScore(coverageConfiguration, codeCoverageRatio);
+
+        assertThat(coverageScore.getMissedSize()).isEqualTo(100 - codeCoverageRatio.getPercentage());
+    }
+
+    private CoverageConfiguration createCoverageConfiguration(final int missedImpact, final int coveredImpact) {
+        return new CoverageConfigurationBuilder()
+                .setMissedImpact(missedImpact)
+                .setCoveredImpact(coveredImpact)
+                .build();
     }
 }
