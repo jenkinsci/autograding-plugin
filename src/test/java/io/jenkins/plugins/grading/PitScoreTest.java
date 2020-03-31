8a928a143028fbe602de0cdde691adaf90d05324
Kevin Richter <richter_kevin@hotmail.de>
2020-03-31 21:51:43 +0200

    add test with multiple simple configs

--- src/test/java/io/jenkins/plugins/grading/PitScoreTest.java
+++ src/test/java/io/jenkins/plugins/grading/PitScoreTest.java
@@ -108,6 +108,42 @@ class PitScoreTest {
         assertThat(pits.getRatio()).isEqualTo(50);
     }
 
+    @Test
+    void shouldCreateSimplePitScores() {
+        PitConfiguration pitConfiguration = createConfiguration(45, -10, 10, 10);
+        PitBuildAction pitBuildAction = createAction(80, 20);
+        PitScore pits = new PitScore(pitConfiguration, pitBuildAction);
+
+        assertThat(pits.getId()).isEqualTo(pitBuildAction.getDisplayName());
+        assertThat(pits.getTotalImpact()).isEqualTo(650);
+        assertThat(pits.getMutationsSize()).isEqualTo(80);
+        assertThat(pits.getUndetectedSize()).isEqualTo(20);
+        assertThat(pits.getDetectedSize()).isEqualTo(60);
+        assertThat(pits.getRatio()).isEqualTo(25);
+
+        PitConfiguration pitConfiguration2 = createConfiguration(10, 0, 5, 0);
+        PitBuildAction pitBuildAction2 = createAction(50, 10);
+        PitScore pits2 = new PitScore(pitConfiguration2, pitBuildAction2);
+
+        assertThat(pits2.getId()).isEqualTo(pitBuildAction2.getDisplayName());
+        assertThat(pits2.getTotalImpact()).isEqualTo(200);
+        assertThat(pits2.getMutationsSize()).isEqualTo(50);
+        assertThat(pits2.getUndetectedSize()).isEqualTo(10);
+        assertThat(pits2.getDetectedSize()).isEqualTo(40);
+        assertThat(pits2.getRatio()).isEqualTo(20);
+
+        PitConfiguration pitConfiguration3 = createConfiguration(9000, 500, -500, 10);
+        PitBuildAction pitBuildAction3 = createAction(80, 20);
+        PitScore pits3 = new PitScore(pitConfiguration3, pitBuildAction3);
+
+        assertThat(pits3.getId()).isEqualTo(pitBuildAction3.getDisplayName());
+        assertThat(pits3.getTotalImpact()).isEqualTo(-19750);
+        assertThat(pits3.getMutationsSize()).isEqualTo(80);
+        assertThat(pits3.getUndetectedSize()).isEqualTo(20);
+        assertThat(pits3.getDetectedSize()).isEqualTo(60);
+        assertThat(pits3.getRatio()).isEqualTo(25);
+    }
+
     private PitConfiguration createConfiguration(final int maxScore,
             final int undetectedImpact,
             final int detectedImpact,
