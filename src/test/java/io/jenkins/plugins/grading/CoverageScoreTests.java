0ad5b252bca43c13ee84e0b47a6e0071f5e82606
Andis Surface <andreas.riepl@gmx.net>
2020-03-31 19:36:09 +0200

    Create Tests for CoverageScore

--- src/test/java/io/jenkins/plugins/grading/CoverageScoreTests.java
+++ src/test/java/io/jenkins/plugins/grading/CoverageScoreTests.java
@@ -2,8 +2,6 @@ package io.jenkins.plugins.grading;
 
 import org.junit.jupiter.api.Test;
 
-import hudson.model.TaskListener;
-
 import io.jenkins.plugins.coverage.targets.Ratio;
 import io.jenkins.plugins.grading.CoverageConfiguration.CoverageConfigurationBuilder;
 
@@ -14,15 +12,45 @@ import static io.jenkins.plugins.grading.assertions.Assertions.*;
  *
  * @author Eva-Maria Zeintl
  * @author Ullrich Hafner
+ * @author Andreas Riepl
  */
 class CoverageScoreTests {
+
     @Test
-    void shouldCalculate() {
+    void givenCoverageScore_whenRatio99Percent_thenTotalImpactShouldEqualImpactValue() {
+        int missedImpactValue = -2;
+
         CoverageConfiguration coverageConfiguration = new CoverageConfigurationBuilder()
-                .setMissedImpact(-2)
+                .setMissedImpact(missedImpactValue)
                 .build();
         CoverageScore coverageScore = new CoverageScore(coverageConfiguration, Ratio.create(99, 100));
 
-        assertThat(coverageScore).hasTotalImpact(-2);
+        assertThat(coverageScore).hasTotalImpact(missedImpactValue);
     }
+
+    @Test
+    void givenCoverageScore_whenRatio90Percent_thenTotalImpactShouldBeTenTimesHigher() {
+        int missedImpactValue = 6;
+
+        CoverageConfiguration coverageConfiguration = new CoverageConfigurationBuilder()
+                .setMissedImpact(missedImpactValue)
+                .build();
+        CoverageScore coverageScore = new CoverageScore(coverageConfiguration, Ratio.create(90, 100));
+
+        assertThat(coverageScore).hasTotalImpact(missedImpactValue*10);
+    }
+
+    @Test
+    void givenCoverageScore_whenRatio1Percent_thenTotalImpactShouldBe99TimesHigher() {
+        int missedImpactValue = -869;
+
+        CoverageConfiguration coverageConfiguration = new CoverageConfigurationBuilder()
+                .setMissedImpact(missedImpactValue)
+                .build();
+        CoverageScore coverageScore = new CoverageScore(coverageConfiguration, Ratio.create(1, 100));
+
+        assertThat(coverageScore).hasTotalImpact(missedImpactValue*99);
+    }
+
+
 }
