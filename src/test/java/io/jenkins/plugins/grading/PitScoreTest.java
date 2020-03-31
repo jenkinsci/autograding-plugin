9205a1eddfe1974b94bc114845251a53f719cdfc
Kevin Richter <richter_kevin@hotmail.de>
2020-03-31 21:33:13 +0200

    improve tests for PitScore

--- src/test/java/io/jenkins/plugins/grading/PitScoreTest.java
+++ src/test/java/io/jenkins/plugins/grading/PitScoreTest.java
@@ -14,11 +14,12 @@ import static org.mockito.Mockito.*;
  *
  * @author Eva-Maria Zeintl
  * @author Ullrich Hafner
+ * @author Kevin Richter
  */
 class PitScoreTest {
     @Test
     void shouldCalculateSizeImpacts() {
-        PitConfiguration pitConfiguration = createConfiguration();
+        PitConfiguration pitConfiguration = createConfiguration(25, -2, 1, 0);
 
         PitScore pits = new PitScore(pitConfiguration, createAction(30, 5));
 
@@ -27,9 +28,7 @@ class PitScoreTest {
 
     @Test
     void shouldCalculateRatioImpacts() {
-        PitConfiguration pitConfiguration = new PitConfiguration.PitConfigurationBuilder().setMaxScore(20)
-                .setRatioImpact(-2)
-                .build();
+        PitConfiguration pitConfiguration = createConfiguration(20, 0, 0, -2);
 
         PitScore pits = new PitScore(pitConfiguration, createAction(30, 3));
 
@@ -38,17 +37,85 @@ class PitScoreTest {
 
     @Test
     void shouldCalculateNegativeResult() {
-        PitConfiguration pitConfiguration = createConfiguration();
+        PitConfiguration pitConfiguration = createConfiguration(25, -2, 1, 0);
 
         PitScore pits = new PitScore(pitConfiguration, createAction(30, 20));
 
-        assertThat(pits.getTotalImpact()).isEqualTo(-30);
+        assertThat(pits).hasTotalImpact(-30);
     }
 
-    private PitConfiguration createConfiguration() {
-        return new PitConfiguration.PitConfigurationBuilder().setMaxScore(25)
-                .setUndetectedImpact(-2)
-                .setDetectedImpact(1)
+    @Test
+    void shouldCalculateZeroTotalImpact() {
+        PitConfiguration pitConfiguration = createConfiguration(25, 0, 0, 0);
+
+        PitScore pits = new PitScore(pitConfiguration, createAction(30, 20));
+
+        assertThat(pits).hasTotalImpact(0);
+    }
+
+    @Test
+    void shouldGetBuildActionId() {
+        PitConfiguration pitConfiguration = createConfiguration(0, 0, 0, 0);
+        PitBuildAction pitBuildAction = createAction(1, 0);
+
+        PitScore pits = new PitScore(pitConfiguration, pitBuildAction);
+
+        assertThat(pits.getId()).isEqualTo(pitBuildAction.getDisplayName());
+    }
+
+    @Test
+    void shouldGetMutationSize() {
+        PitConfiguration pitConfiguration = createConfiguration(0, 0, 0, 0);
+        PitBuildAction pitBuildAction = createAction(1, 0);
+
+        PitScore pits = new PitScore(pitConfiguration, pitBuildAction);
+
+        assertThat(pits.getMutationsSize()).isEqualTo(1);
+        assertThat(pits.getDetectedSize()).isEqualTo(1);
+        assertThat(pits.getRatio()).isEqualTo(0);
+    }
+
+    @Test
+    void shouldGetUndetectedSize() {
+        PitConfiguration pitConfiguration = createConfiguration(0, 0, 0, 0);
+        PitBuildAction pitBuildAction = createAction(1, 20);
+
+        PitScore pits = new PitScore(pitConfiguration, pitBuildAction);
+
+        assertThat(pits.getUndetectedSize()).isEqualTo(20);
+    }
+
+    @Test
+    void shouldGetDetectedSize() {
+        PitConfiguration pitConfiguration = createConfiguration(0, 0, 0, 0);
+        PitBuildAction pitBuildAction = createAction(30, 20);
+
+        PitScore pits = new PitScore(pitConfiguration, pitBuildAction);
+
+        assertThat(pits.getDetectedSize()).isEqualTo(10);
+    }
+
+    @Test
+    void shouldGetRatio() {
+        PitConfiguration pitConfiguration = createConfiguration(0, 0, 0, 0);
+        PitBuildAction pitBuildAction = createAction(100, 50);
+
+        PitScore pits = new PitScore(pitConfiguration, pitBuildAction);
+
+        assertThat(pits.getMutationsSize()).isEqualTo(100);
+        assertThat(pits.getUndetectedSize()).isEqualTo(50);
+        assertThat(pits.getDetectedSize()).isEqualTo(50);
+        assertThat(pits.getRatio()).isEqualTo(50);
+    }
+
+    private PitConfiguration createConfiguration(final int maxScore,
+            final int undetectedImpact,
+            final int detectedImpact,
+            final int ratioImpact) {
+        return new PitConfiguration.PitConfigurationBuilder().setMaxScore(maxScore)
+                .setUndetectedImpact(undetectedImpact)
+                .setDetectedImpact(detectedImpact)
+                .setRatioImpact(ratioImpact)
                 .build();
     }
 
@@ -60,6 +127,7 @@ class PitScoreTest {
         when(stats.getUndetected()).thenReturn(undetectedSize);
         when(mutations.getMutationStats()).thenReturn(stats);
         when(action.getReport()).thenReturn(mutations);
+        when(action.getDisplayName()).thenReturn("pit-build-action");
         return action;
     }
 }
