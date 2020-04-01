dad2937a37c95b26a3b7548e9aba95608eb6f6d5
Patrick Rogg <p_rogg@t-online.de>
2020-04-01 12:31:13 +0200

    Combine getter Tests and remove finals

--- src/test/java/io/jenkins/plugins/grading/CoverageScoreTests.java
+++ src/test/java/io/jenkins/plugins/grading/CoverageScoreTests.java
@@ -18,62 +18,37 @@ class CoverageScoreTests {
 
     @Test
     void shouldCalculateTotalImpactWithZeroCoveredImpact() {
-        final CoverageConfiguration coverageConfiguration = createCoverageConfiguration(-2, 0);
-        final CoverageScore coverageScore = new CoverageScore(coverageConfiguration, Ratio.create(99, 100));
+        CoverageConfiguration coverageConfiguration = createCoverageConfiguration(-2, 0);
+        CoverageScore coverageScore = new CoverageScore(coverageConfiguration, Ratio.create(99, 100));
 
         assertThat(coverageScore).hasTotalImpact(-2);
     }
 
     @Test
     void shouldCalculateTotalImpactWithZeroMissedImpact() {
-        final CoverageConfiguration coverageConfiguration = createCoverageConfiguration(0, 5);
-        final CoverageScore coverageScore = new CoverageScore(coverageConfiguration, Ratio.create(99, 100));
+        CoverageConfiguration coverageConfiguration = createCoverageConfiguration(0, 5);
+        CoverageScore coverageScore = new CoverageScore(coverageConfiguration, Ratio.create(99, 100));
 
         assertThat(coverageScore).hasTotalImpact(495);
     }
 
     @Test
     void shouldCalculateTotalImpact() {
-        final CoverageConfiguration coverageConfiguration = createCoverageConfiguration(-1, 3);
-        final CoverageScore coverageScore = new CoverageScore(coverageConfiguration, Ratio.create(99, 100));
+        CoverageConfiguration coverageConfiguration = createCoverageConfiguration(-1, 3);
+        CoverageScore coverageScore = new CoverageScore(coverageConfiguration, Ratio.create(99, 100));
 
         assertThat(coverageScore).hasTotalImpact(296);
     }
 
     @Test
-    void shouldGetId() {
-        final CoverageConfiguration coverageConfiguration = new CoverageConfigurationBuilder().build();
-        final CoverageScore coverageScore = new CoverageScore(coverageConfiguration, Ratio.create(99, 100));
+    void shouldGetProperties() {
+        Ratio codeCoverageRatio = Ratio.create(99, 100);
+        CoverageConfiguration coverageConfiguration = createCoverageConfiguration(1, 1);
+        CoverageScore coverageScore = new CoverageScore(coverageConfiguration, Ratio.create(99, 100));
 
-        assertThat(coverageScore.getId()).isEqualTo("Line");
-    }
-
-    @Test
-    void shouldGetTotalImpact() {
-        final CoverageConfiguration coverageConfiguration = createCoverageConfiguration(1, 1);
-        final CoverageScore coverageScore = new CoverageScore(coverageConfiguration, Ratio.create(99, 100));
-
-        final int missedImpact = coverageConfiguration.getMissedImpact() * coverageScore.getMissedSize();
-        final int coveredImpact = coverageConfiguration.getCoveredImpact() * coverageScore.getCoveredSize();
-        assertThat(coverageScore.getTotalImpact()).isEqualTo(missedImpact + coveredImpact);
-    }
-
-    @Test
-    void shouldGetCoveredSize() {
-        final Ratio codeCoverageRatio = Ratio.create(99, 100);
-        final CoverageConfiguration coverageConfiguration = new CoverageConfigurationBuilder().build();
-        final CoverageScore coverageScore = new CoverageScore(coverageConfiguration, codeCoverageRatio);
-
-        assertThat(coverageScore.getCoveredSize()).isEqualTo(codeCoverageRatio.getPercentage());
-    }
-
-    @Test
-    void shouldGetMissedSize() {
-        final Ratio codeCoverageRatio = Ratio.create(99, 100);
-        final CoverageConfiguration coverageConfiguration = new CoverageConfigurationBuilder().build();
-        final CoverageScore coverageScore = new CoverageScore(coverageConfiguration, codeCoverageRatio);
-
-        assertThat(coverageScore.getMissedSize()).isEqualTo(100 - codeCoverageRatio.getPercentage());
+        assertThat(coverageScore).hasId("Line");
+        assertThat(coverageScore).hasCoveredSize(codeCoverageRatio.getPercentage());
+        assertThat(coverageScore).hasMissedSize(100 - codeCoverageRatio.getPercentage());
     }
 
     private CoverageConfiguration createCoverageConfiguration(final int missedImpact, final int coveredImpact) {
