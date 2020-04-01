c7d41b8833c5d5ce4dd8ade1f00fa6db29dc2742
Johannes Hintermaier <mojo.johannes@arcor.de>
2020-04-01 18:13:26 +0200

    add 2 more tests for coverageConfiguration JSON input

--- src/test/java/io/jenkins/plugins/grading/CoverageScoreTests.java
+++ src/test/java/io/jenkins/plugins/grading/CoverageScoreTests.java
@@ -27,12 +27,34 @@ class CoverageScoreTests {
 
         assertThat(coverageScore).hasTotalImpact(-2);
     }
+
     @Test
     void shouldConvertFromJson() {
         CoverageConfiguration configuration = CoverageConfiguration.from(JSONObject.fromObject("{\"maxScore\": 4, \"coveredImpact\":5, \"missedImpact\":3}"));
         assertThat(configuration).hasMaxScore(4);
         assertThat(configuration).hasCoveredImpact(5);
         assertThat(configuration).hasMissedImpact(3);
+    }
+
+    @Test
+    void shouldInitializeWithDefault(){
+        CoverageConfiguration configurationEmpty = CoverageConfiguration.from(JSONObject.fromObject("{}"));
+        assertThat(configurationEmpty).hasMaxScore(0);
+        assertThat(configurationEmpty).hasCoveredImpact(0);
+        assertThat(configurationEmpty).hasMissedImpact(0);
+
+        CoverageConfiguration configurationOneValue = CoverageConfiguration.from(JSONObject.fromObject("{\"maxScore\": 4}"));
+        assertThat(configurationOneValue).hasMaxScore(4);
+        assertThat(configurationOneValue).hasCoveredImpact(0);
+        assertThat(configurationOneValue).hasMissedImpact(0);
+    }
+
+    @Test
+    void shouldNotReadAdditionalAttributes(){
+        CoverageConfiguration configuration = CoverageConfiguration.from(JSONObject.fromObject("{\"maxScore\": 2, \"coveredImpact\":3, \"missedImpact\":4, \"notRead\":5}"));
+        assertThat(configuration).hasMaxScore(2);
+        assertThat(configuration).hasCoveredImpact(3);
+        assertThat(configuration).hasMissedImpact(4);
     }
 
 }
