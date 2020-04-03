e8739e90d2268122c3ab724becb634688203bac2
Ulli Hafner <ullrich.hafner@gmail.com>
2020-04-03 20:49:25 +0200

    Merge with master.

--- src/test/java/io/jenkins/plugins/grading/TestScoreTest.java
+++ src/test/java/io/jenkins/plugins/grading/TestScoreTest.java
@@ -3,6 +3,7 @@ package io.jenkins.plugins.grading;
 import java.util.Arrays;
 import java.util.Collection;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.runners.Parameterized.Parameters;
@@ -11,9 +12,8 @@ import net.sf.json.JSONObject;
 
 import hudson.tasks.junit.TestResultAction;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static io.jenkins.plugins.grading.assertions.Assertions.assertThat;
 
 /**
  * Tests the class {@link TestScore}.
@@ -26,46 +26,47 @@ class TestScoreTest {
     @Parameters
     private static Collection<Object[]> createTestConfigurationParameters() {
         return Arrays.asList(new Object[][] {
-            {
-                createTestConfiguration(25,-1, -2, 1),
-                createAction(8, 1, 1),
-                3
-            },
-            {
-                createTestConfiguration(25,-1, -2, 1),
-                createAction(8, 5, 1),
-                -9
-            },
-            {
-                createTestConfiguration(25,-1, -2, -1),
-                createAction(8, 5, 1),
-                -13
-            },
-            {
-                createTestConfiguration(25,0, 0, 0),
-                createAction(0, 0, 0),
-                0
-            },
-            {
-                createTestConfiguration(25,99, 99, 99),
-                createAction(0, 0, 0),
-                0
-            },
-            {
-                createTestConfiguration(25,1, 1, 1),
-                createAction(3, 3, 0),
-                3
-            },
+                {
+                        createTestConfiguration(25, -1, -2, 1),
+                        createAction(8, 1, 1),
+                        3
+                },
+                {
+                        createTestConfiguration(25, -1, -2, 1),
+                        createAction(8, 5, 1),
+                        -9
+                },
+                {
+                        createTestConfiguration(25, -1, -2, -1),
+                        createAction(8, 5, 1),
+                        -13
+                },
+                {
+                        createTestConfiguration(25, 0, 0, 0),
+                        createAction(0, 0, 0),
+                        0
+                },
+                {
+                        createTestConfiguration(25, 99, 99, 99),
+                        createAction(0, 0, 0),
+                        0
+                },
+                {
+                        createTestConfiguration(25, 1, 1, 1),
+                        createAction(3, 3, 0),
+                        3
+                },
         });
     }
 
     @ParameterizedTest
     @MethodSource("createTestConfigurationParameters")
     void shouldComputeTestScoreWith(final TestConfiguration configuration,
-            final TestResultAction resultAction,  final int expectedTotalImpact) {
+            final TestResultAction resultAction, final int expectedTotalImpact) {
         TestScore test = new TestScore(configuration, resultAction);
         assertThat(test.getTotalSize()).isEqualTo(resultAction.getTotalCount());
-        assertThat(test.getPassedSize()).isEqualTo(resultAction.getTotalCount() - resultAction.getFailCount() - resultAction.getSkipCount());
+        assertThat(test.getPassedSize()).isEqualTo(
+                resultAction.getTotalCount() - resultAction.getFailCount() - resultAction.getSkipCount());
         assertThat(test.getFailedSize()).isEqualTo(resultAction.getFailCount());
         assertThat(test.getSkippedSize()).isEqualTo(resultAction.getSkipCount());
         assertThat(test.getId()).isEqualTo(resultAction.getDisplayName());
@@ -75,11 +76,11 @@ class TestScoreTest {
     private static TestConfiguration createTestConfiguration(
             final int maxScore, final int skippedImpact, final int failureImpact, final int passedImpact) {
         return new TestConfiguration.TestConfigurationBuilder()
-            .setMaxScore(maxScore)
-            .setSkippedImpact(skippedImpact)
-            .setFailureImpact(failureImpact)
-            .setPassedImpact(passedImpact)
-            .build();
+                .setMaxScore(maxScore)
+                .setSkippedImpact(skippedImpact)
+                .setFailureImpact(failureImpact)
+                .setPassedImpact(passedImpact)
+                .build();
     }
 
     private static TestResultAction createAction(final int totalSize, final int failedSize, final int skippedSize) {
@@ -96,7 +97,7 @@ class TestScoreTest {
                 "{}"));
 
         assertThat(configuration).hasMaxScore(0);
-        assertThat(configuration).hasFailureImpact((0));
+        assertThat(configuration).hasFailureImpact(0);
         assertThat(configuration).hasPassedImpact(0);
         assertThat(configuration).hasSkippedImpact(0);
     }
@@ -107,7 +108,7 @@ class TestScoreTest {
                 "{\"additionalAttribute\":5}"));
 
         assertThat(configuration).hasMaxScore(0);
-        assertThat(configuration).hasFailureImpact((0));
+        assertThat(configuration).hasFailureImpact(0);
         assertThat(configuration).hasPassedImpact(0);
         assertThat(configuration).hasSkippedImpact(0);
     }
@@ -118,7 +119,7 @@ class TestScoreTest {
                 "{\"maxScore\":5,\"failureImpact\":1,\"passedImpact\":2,\"skippedImpact\":3}"));
 
         assertThat(configuration).hasMaxScore(5);
-        assertThat(configuration).hasFailureImpact((1));
+        assertThat(configuration).hasFailureImpact(1);
         assertThat(configuration).hasPassedImpact(2);
         assertThat(configuration).hasSkippedImpact(3);
     }
