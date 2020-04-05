34161eff052b44c2e80f3c30b7bbae282617d306
Johannes Hintermaier <mojo.johannes@arcor.de>
2020-04-05 16:25:05 +0200

    merge with other coverafeScoreTests

--- src/test/java/io/jenkins/plugins/grading/CoverageScoreTests.java
+++ src/test/java/io/jenkins/plugins/grading/CoverageScoreTests.java
@@ -2,31 +2,63 @@ package io.jenkins.plugins.grading;
 
 import org.junit.jupiter.api.Test;
 
-import net.sf.json.JSONObject;
-
-import hudson.model.TaskListener;
-
 import io.jenkins.plugins.coverage.targets.Ratio;
 import io.jenkins.plugins.grading.CoverageConfiguration.CoverageConfigurationBuilder;
 
 import static io.jenkins.plugins.grading.assertions.Assertions.*;
+
+import net.sf.json.JSONObject;
 
 /**
  * Tests the class {@link CoverageScore}.
  *
  * @author Eva-Maria Zeintl
  * @author Ullrich Hafner
+ * @author Patrick Rogg
  * @author Johannes Hintermaier
  */
 class CoverageScoreTests {
+
     @Test
-    void shouldCalculate() {
-        CoverageConfiguration coverageConfiguration = new CoverageConfigurationBuilder()
-                .setMissedImpact(-2)
-                .build();
+    void shouldCalculateTotalImpactWithZeroCoveredImpact() {
+        CoverageConfiguration coverageConfiguration = createCoverageConfiguration(-2, 0);
         CoverageScore coverageScore = new CoverageScore(coverageConfiguration, Ratio.create(99, 100));
 
         assertThat(coverageScore).hasTotalImpact(-2);
+    }
+
+    @Test
+    void shouldCalculateTotalImpactWithZeroMissedImpact() {
+        CoverageConfiguration coverageConfiguration = createCoverageConfiguration(0, 5);
+        CoverageScore coverageScore = new CoverageScore(coverageConfiguration, Ratio.create(99, 100));
+
+        assertThat(coverageScore).hasTotalImpact(495);
+    }
+
+    @Test
+    void shouldCalculateTotalImpact() {
+        CoverageConfiguration coverageConfiguration = createCoverageConfiguration(-1, 3);
+        CoverageScore coverageScore = new CoverageScore(coverageConfiguration, Ratio.create(99, 100));
+
+        assertThat(coverageScore).hasTotalImpact(296);
+    }
+
+    @Test
+    void shouldGetProperties() {
+        Ratio codeCoverageRatio = Ratio.create(99, 100);
+        CoverageConfiguration coverageConfiguration = createCoverageConfiguration(1, 1);
+        CoverageScore coverageScore = new CoverageScore(coverageConfiguration, codeCoverageRatio);
+
+        assertThat(coverageScore).hasId("Line");
+        assertThat(coverageScore).hasCoveredSize(codeCoverageRatio.getPercentage());
+        assertThat(coverageScore).hasMissedSize(100 - codeCoverageRatio.getPercentage());
+    }
+
+    private CoverageConfiguration createCoverageConfiguration(final int missedImpact, final int coveredImpact) {
+        return new CoverageConfigurationBuilder()
+                .setMissedImpact(missedImpact)
+                .setCoveredImpact(coveredImpact)
+                .build();
     }
 
     @Test
@@ -57,5 +89,7 @@ class CoverageScoreTests {
         assertThat(configuration).hasCoveredImpact(3);
         assertThat(configuration).hasMissedImpact(4);
     }
+
+
 
 }
