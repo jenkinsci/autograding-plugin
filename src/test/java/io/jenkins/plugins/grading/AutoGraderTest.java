642ef9887518c31c3fdb5925a8ee8dbedff585f5
Mitja <m.oldenbourg@hm.edu>
2020-04-20 00:33:23 +0200

    Add shouldLogScoreFromRecordedTestResults (wip - not working)

--- src/test/java/io/jenkins/plugins/grading/AutoGraderTest.java
+++ src/test/java/io/jenkins/plugins/grading/AutoGraderTest.java
@@ -1,5 +1,7 @@
 package io.jenkins.plugins.grading;
 
+import java.io.ByteArrayOutputStream;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +10,7 @@ import hudson.Launcher;
 import hudson.model.Run;
 import hudson.model.TaskListener;
 import hudson.remoting.VirtualChannel;
+import hudson.tasks.junit.TestResultAction;
 
 import static org.mockito.Mockito.*;
 
@@ -37,4 +40,42 @@ class AutoGraderTest {
                         TaskListener.NULL));
     }
 
+    @Test
+    void shouldLogScoreFromRecordedTestResults() {
+        String testConfiguration = "{ 'tests' : "
+                + "{ "
+                + "'passedImpact' : 1, "
+                + "'failureImpact' : 1, "
+                + "'skippedImpact' : 1, "
+                + "'maxScore' : 1 "
+                + "} "
+                + "}";
+        String displayName = "testName";
+        AutoGrader autoGrader = new AutoGrader(testConfiguration);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        TestResultAction action = mock(TestResultAction.class);
+        when(action.getFailCount()).thenReturn(1);
+        when(action.getSkipCount()).thenReturn(1);
+        when(action.getTotalCount()).thenReturn(5);
+        when(action.getDisplayName()).thenReturn(displayName);
+
+        AggregatedScore score = mock(AggregatedScore.class);
+        when(score.addTestsTotal(any(), any())).thenReturn(1);
+
+        TestScore testScore = mock(TestScore.class);
+        when(testScore.getTotalImpact()).thenReturn(1);
+
+        Run run = mock(Run.class);
+        when(run.getAction(any())).thenReturn(action);
+
+        autoGrader.perform(run, new FilePath((VirtualChannel) null, "/"), mock(Launcher.class),
+                TaskListener.NULL);
+
+
+        Assertions.assertThat(outputStream.toString())
+                .isEqualTo(String.format("Grading test results [%s]\n"
+                        + "-> Score [%d] - from recorded test results: [%d], [%d], [%d], [%d]\n"
+                        + "Total score for test results: [%s]",displayName, 1, 1, 1, 1, 1, 1));
+    }
 }
