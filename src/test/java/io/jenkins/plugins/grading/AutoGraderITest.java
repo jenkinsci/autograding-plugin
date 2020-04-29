e87952371125559c28126c82d7b333960a1a8eef
ThoGro <grossbec@hm.edu>
2020-04-29 20:58:26 +0200

    Add integration tests for AutoGrader

--- src/test/java/io/jenkins/plugins/grading/AutoGraderITest.java
+++ src/test/java/io/jenkins/plugins/grading/AutoGraderITest.java
@@ -19,6 +19,7 @@ import static io.jenkins.plugins.grading.assertions.Assertions.*;
  * Integration tests for the {@link AutoGrader} step.
  *
  * @author Ullrich Hafner
+ * @author Thomas Großbeck
  */
 public class AutoGraderITest extends IntegrationTestWithJenkinsPerSuite {
     /** Verifies that the step skips all autograding parts if the configuration is empty. */
@@ -69,6 +70,126 @@ public class AutoGraderITest extends IntegrationTestWithJenkinsPerSuite {
         assertThat(score).hasAchieved(40);
     }
 
+    @Test
+    public void shouldCalculateStaticAnalysisResults() {
+        WorkflowJob job = createPipelineWithWorkspaceFiles("pmd.xml");
+
+        configureScanner(job, "pmd",
+                "{\"analysis\":{\"maxScore\":100,\"errorImpact\":-10,\"highImpact\":-5,\"normalImpact\":-2,\"lowImpact\":-1}}");
+        Run<?, ?> baseline = buildSuccessfully(job);
+
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] Grading static analysis results for PMD");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] Skipping test results");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] Skipping coverage results");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] Skipping mutation coverage results");
+
+        List<AutoGradingBuildAction> actions = baseline.getActions(AutoGradingBuildAction.class);
+        assertThat(actions).hasSize(1);
+        AggregatedScore score = actions.get(0).getResult();
+        assertThat(score).hasAchieved(96);
+    }
+
+    @Test
+    public void shouldCalculateTestResults() {
+        WorkflowJob job = createPipelineWithWorkspaceFiles("TEST-io.jenkins.plugins.grading.AggregatedScoreXmlStreamTest.xml",
+                "TEST-io.jenkins.plugins.grading.AnalysisScoreTest.xml",
+                "TEST-io.jenkins.plugins.grading.ArchitectureRulesTest.xml",
+                "TEST-io.jenkins.plugins.grading.AutoGraderTest.xml",
+                "TEST-io.jenkins.plugins.grading.CoverageScoreTests.xml",
+                "TEST-io.jenkins.plugins.grading.PackageArchitectureTest.xml",
+                "TEST-io.jenkins.plugins.grading.PitScoreTest.xml",
+                "TEST-io.jenkins.plugins.grading.ScoreTest.xml",
+                "TEST-io.jenkins.plugins.grading.TestScoreTest.xml");
+
+        configureScanner(job, "test",
+                "{\"tests\":{\"maxScore\":100,\"passedImpact\":1,\"failureImpact\":-5,\"skippedImpact\":-1}}");
+        Run<?, ?> baseline = buildSuccessfully(job);
+
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] Skipping static analysis results");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] Grading test results Test Result");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] -> Score 46 - from recorded test results: 46, 46, 0, 0");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] Skipping coverage results");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] Skipping mutation coverage results");
+
+        List<AutoGradingBuildAction> actions = baseline.getActions(AutoGradingBuildAction.class);
+        assertThat(actions).hasSize(1);
+        AggregatedScore score = actions.get(0).getResult();
+        assertThat(score).hasAchieved(46);
+    }
+
+    @Test
+    public void shouldCalculateCoverageResults() {
+        WorkflowJob job = createPipelineWithWorkspaceFiles("jacoco.xml");
+
+        configureScanner(job, "jacoco",
+                "{\"coverage\":{\"maxScore\":100,\"coveredImpact\":1,\"missedImpact\":-1}}");
+        Run<?, ?> baseline = buildSuccessfully(job);
+
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] Skipping static analysis results");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] Skipping test results");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] Grading coverage results Coverage Report");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] -> Score 64 - from recorded line coverage results: 82%");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] -> Score -8 - from recorded branch coverage results: 46%");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] Skipping mutation coverage results");
+
+        List<AutoGradingBuildAction> actions = baseline.getActions(AutoGradingBuildAction.class);
+        assertThat(actions).hasSize(1);
+        AggregatedScore score = actions.get(0).getResult();
+        assertThat(score).hasAchieved(56);
+    }
+
+    @Test
+    public void shouldCalculatePitResults() {
+        WorkflowJob job = createPipelineWithWorkspaceFiles("mutations.xml");
+
+        configureScanner(job, "mutations",
+                "{\"pit\":{\"maxScore\":100,\"detectedImpact\":1,\"undetectedImpact\":-1,\"ratioImpact\":0}}");
+        Run<?, ?> baseline = buildSuccessfully(job);
+
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] Skipping static analysis results");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] Skipping test results");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] Skipping coverage results");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] Grading PIT mutation results PIT Mutation Report");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] -> Score 87 - from recorded PIT mutation results: 191, 52, 139, 28");
+
+        List<AutoGradingBuildAction> actions = baseline.getActions(AutoGradingBuildAction.class);
+        assertThat(actions).hasSize(1);
+        AggregatedScore score = actions.get(0).getResult();
+        assertThat(score).hasAchieved(87);
+    }
+
+    @Test
+    public void shouldCalculateFullResults() {
+        WorkflowJob job = createPipelineWithWorkspaceFiles("pmd.xml", "jacoco.xml", "mutations.xml",
+                "TEST-io.jenkins.plugins.grading.AggregatedScoreXmlStreamTest.xml",
+                "TEST-io.jenkins.plugins.grading.AnalysisScoreTest.xml",
+                "TEST-io.jenkins.plugins.grading.ArchitectureRulesTest.xml",
+                "TEST-io.jenkins.plugins.grading.AutoGraderTest.xml",
+                "TEST-io.jenkins.plugins.grading.CoverageScoreTests.xml",
+                "TEST-io.jenkins.plugins.grading.PackageArchitectureTest.xml",
+                "TEST-io.jenkins.plugins.grading.PitScoreTest.xml",
+                "TEST-io.jenkins.plugins.grading.ScoreTest.xml",
+                "TEST-io.jenkins.plugins.grading.TestScoreTest.xml");
+
+        configureScannerForFullGrading(job,
+                "{\"analysis\":{\"maxScore\":100,\"errorImpact\":-10,\"highImpact\":-5,\"normalImpact\":-2,\"lowImpact\":-1}, \"tests\":{\"maxScore\":100,\"passedImpact\":1,\"failureImpact\":-5,\"skippedImpact\":-1}, \"coverage\":{\"maxScore\":100,\"coveredImpact\":1,\"missedImpact\":-1}, \"pit\":{\"maxScore\":100,\"detectedImpact\":1,\"undetectedImpact\":-1,\"ratioImpact\":0}}");
+        Run<?, ?> baseline = buildSuccessfully(job);
+
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] Grading static analysis results for PMD");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] Grading test results Test Result");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] -> Score 46 - from recorded test results: 46, 46, 0, 0");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] Grading coverage results Coverage Report");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] -> Score 64 - from recorded line coverage results: 82%");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] -> Score -8 - from recorded branch coverage results: 46%");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] Grading PIT mutation results PIT Mutation Report");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] -> Score 87 - from recorded PIT mutation results: 191, 52, 139, 28");
+
+        List<AutoGradingBuildAction> actions = baseline.getActions(AutoGradingBuildAction.class);
+        assertThat(actions).hasSize(1);
+        AggregatedScore score = actions.get(0).getResult();
+        assertThat(score).hasAchieved(285);
+    }
+
     /**
      * Returns the console log as a String.
      *
@@ -86,13 +207,44 @@ public class AutoGraderITest extends IntegrationTestWithJenkinsPerSuite {
         }
     }
 
-    private void configureScanner(final WorkflowJob job, final String fileName, final String configuration) {
+    private void configureScannerForFullGrading(final WorkflowJob job, final String configuration) {
         job.setDefinition(new CpsFlowDefinition("node {\n"
                 + "  stage ('Integration Test') {\n"
-                + "         recordIssues tool: checkStyle(pattern: '**/" + fileName + "*')\n"
+                + "         recordIssues tool: pmdParser(pattern: '**/pmd.xml')\n"
+                + "         junit testResults: '**/TEST-*.xml'\n"
+                + "         publishCoverage adapters: [jacocoAdapter('**/jacoco.xml')]\n"
+                + "         step([$class: 'PitPublisher', mutationStatsFile: '**/mutations.xml'])\n"
                 + "         autoGrade('" + configuration + "')\n"
                 + "  }\n"
                 + "}", true));
+    }
+
+    private void configureScanner(final WorkflowJob job, final String fileName, final String configuration) {
+        String definition = "node {\n"
+                + "  stage ('Integration Test') {\n";
+        switch(fileName) {
+            case "checkstyle":
+                definition += "         recordIssues tool: checkStyle(pattern: '**/" + fileName + "*')\n";
+                break;
+            case "pmd":
+                definition += "         recordIssues tool: pmdParser(pattern: '**/" + fileName + "*')\n";
+                break;
+            case "test":
+                definition += "         junit testResults: '**/TEST-*.xml'\n";
+                break;
+            case "jacoco":
+                definition += "         publishCoverage adapters: [jacocoAdapter('**/" + fileName + "*')]\n";
+                break;
+            case "mutations":
+                definition += "         step([$class: 'PitPublisher', mutationStatsFile: '**/" + fileName + "*'])\n";
+                break;
+            default:
+                break;
+        }
+        definition += "         autoGrade('" + configuration + "')\n"
+                + "  }\n"
+                + "}";
+        job.setDefinition(new CpsFlowDefinition(definition, true));
     }
 
 }
