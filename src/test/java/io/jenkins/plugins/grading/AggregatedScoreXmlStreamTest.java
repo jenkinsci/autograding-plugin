0ceca3a4821d2fcffba91c6cde23f9b9146505f6
Ulli Hafner <ullrich.hafner@gmail.com>
2020-04-30 21:39:42 +0200

    Improve mutation coverage of XML reading.

--- src/test/java/io/jenkins/plugins/grading/AggregatedScoreXmlStreamTest.java
+++ src/test/java/io/jenkins/plugins/grading/AggregatedScoreXmlStreamTest.java
@@ -1,5 +1,10 @@
 package io.jenkins.plugins.grading;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import edu.hm.hafner.util.SerializableTest;
@@ -14,18 +19,62 @@ import static io.jenkins.plugins.grading.assertions.Assertions.*;
 public class AggregatedScoreXmlStreamTest extends SerializableTest<AggregatedScore> {
     @Override
     protected AggregatedScore createSerializable() {
-        AggregatedScoreXmlStream xmlStream = new AggregatedScoreXmlStream();
-
-        return xmlStream.read(getResourceAsFile("auto-grading.xml"));
+        return read("auto-grading.xml");
     }
 
     @Test
     void shouldReadVersion1Serialization() {
         AggregatedScore score = createSerializable();
 
+        verifyStream(score);
+    }
+
+    @Test
+    void shouldReturnDefaultForBrokenFile() {
+        AggregatedScore score = read("checkstyle.xml");
+
+        assertThat(score).hasAnalysisAchieved(0).hasTestAchieved(0).hasCoverageAchieved(0).hasPitAchieved(0);
+    }
+
+    @Test
+    void shouldReadAndWriteScores() throws IOException {
+        AggregatedScoreXmlStream reader = new AggregatedScoreXmlStream();
+
+        AggregatedScore restored = createSerializable();
+        Path saved = createTempFile();
+        reader.write(saved, restored);
+
+        List<String> content = Files.readAllLines(saved);
+        assertThat(content).doesNotContain(AnalysisScore.class.getName());
+
+        AggregatedScore written = reader.read(saved);
+        verifyStream(written);
+    }
+
+    private AggregatedScore read(final String fileName) {
+        return new AggregatedScoreXmlStream().read(getResourceAsFile(fileName));
+    }
+
+    private void verifyStream(final AggregatedScore score) {
         assertThat(score).hasAnalysisAchieved(79);
+        assertThat(score.getAnalysisScores()).hasSize(8);
+
         assertThat(score).hasTestAchieved(60);
+        assertThat(score.getTestScores()).hasSize(1);
+
         assertThat(score).hasCoverageAchieved(78);
+        assertThat(score.getCoverageScores()).hasSize(2);
+
         assertThat(score).hasPitAchieved(73);
+        assertThat(score.getPitScores()).hasSize(1);
+    }
+
+    private Path createTempFile() {
+        try {
+            return Files.createTempFile("test", ".test");
+        }
+        catch (IOException exception) {
+            throw new AssertionError(exception);
+        }
     }
 }
