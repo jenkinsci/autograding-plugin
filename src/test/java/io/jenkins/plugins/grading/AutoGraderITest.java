6cd5b8121f79a10f5c46d7aa113a2edaba53ff59
Ulli Hafner <ullrich.hafner@gmail.com>
2020-06-13 23:35:50 +0200

    Revert "Move aggregation code to model so that other libs can reuse it."
    This reverts commit 44e6deb4

--- src/test/java/io/jenkins/plugins/grading/AutoGraderITest.java
+++ src/test/java/io/jenkins/plugins/grading/AutoGraderITest.java
@@ -10,6 +10,7 @@ import edu.hm.hafner.grading.AggregatedScore;
 
 import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
 import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import hudson.model.Result;
 import hudson.model.Run;
 
 import io.jenkins.plugins.util.IntegrationTestWithJenkinsPerSuite;
@@ -32,7 +33,7 @@ public class AutoGraderITest extends IntegrationTestWithJenkinsPerSuite {
 
         assertThat(getConsoleLog(baseline)).contains("[Autograding] Skipping static analysis results");
         assertThat(getConsoleLog(baseline)).contains("[Autograding] Skipping test results");
-        assertThat(getConsoleLog(baseline)).contains("[Autograding] Skipping code coverage results");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] Skipping coverage results");
         assertThat(getConsoleLog(baseline)).contains("[Autograding] Skipping mutation coverage results");
     }
 
@@ -44,11 +45,9 @@ public class AutoGraderITest extends IntegrationTestWithJenkinsPerSuite {
         WorkflowJob job = createPipelineWithWorkspaceFiles("checkstyle.xml");
 
         configureScanner(job, "checkstyle", "{\"tests\":{\"maxScore\":100,\"passedImpact\":1,\"failureImpact\":-5,\"skippedImpact\":-1}}");
-        Run<?, ?> baseline = buildSuccessfully(job);
+        Run<?, ?> baseline = buildWithResult(job, Result.FAILURE);
 
-        assertThat(getConsoleLog(baseline)).contains(
-                "[Autograding] Grading test results",
-                "[Autograding] [-ERROR-] -> Scoring of test results has been enabled, but no results have been found.");
+        assertThat(getConsoleLog(baseline)).contains("java.lang.IllegalArgumentException: Test scoring has been enabled, but no test results have been found.");
     }
 
     /**
@@ -61,10 +60,9 @@ public class AutoGraderITest extends IntegrationTestWithJenkinsPerSuite {
         configureScanner(job, "checkstyle", "{\"analysis\":{\"maxScore\":100,\"errorImpact\":-10,\"highImpact\":-5,\"normalImpact\":-2,\"lowImpact\":-1}}");
         Run<?, ?> baseline = buildSuccessfully(job);
 
-        assertThat(getConsoleLog(baseline)).contains(
-                "[Autograding] Grading static analysis results",
-                "[Autograding] -> CheckStyle score: -60 (errors:6, high:0, normal:0, low:0)",
-                "[Autograding] Total score for static analysis results: 40");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] Grading static analysis results for CheckStyle");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] -> Score -60 (warnings distribution err:6, high:0, normal:0, low:0)");
+        assertThat(getConsoleLog(baseline)).contains("[Autograding] Total score for static analysis results: 40");
 
         List<AutoGradingBuildAction> actions = baseline.getActions(AutoGradingBuildAction.class);
         assertThat(actions).hasSize(1);
